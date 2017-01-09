a6428e6a2ee476f43c1d12b44224f506b7bd8110
Ariel <ariel@digitalminion.com>
2017-01-09 13:52:23 -0800

    fix
    fixed lack of parenthesis and changed 2nd $routeProvider.when to just
    .when

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,19 +2,17 @@ var foolishApp = angular.module('foolishApp', ['ngRoute', 'ngAnimate']);
 
 foolishApp.config(function($routeProvider){
   $routeProvider.when("/",
-    {
-      templateUrl: "assets/tpl/home.html",
-      controller: "ArtboardCtrl",
-      controllerAs: "artboard"
-    }
-
-  $routeProvider.when("/about",
-    {
-      templateUrl: "assets/tpl/about.html",
-      controller: "AboutCtrl",
-      controllerAs: "about"
-    }
-  );
+   {
+     templateUrl: "assets/tpl/home.html",
+     controller: "ArtboardCtrl",
+     controllerAs: "artboard"
+   }).when("/about",
+   {
+     templateUrl: "assets/tpl/about.html",
+     controller: "AboutCtrl",
+     controllerAs: "about"
+   }
+ );
 });
 
 foolishApp.controller('ArtboardCtrl', function() {
