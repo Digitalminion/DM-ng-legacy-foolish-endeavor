3034f88bdcc4b766a4bcb9c9a107aecc280da408
Leahy <leahy@apache.org>
2017-01-23 17:20:34 -0800

    Added counting function
    Counting function is built to test $timeout, function in its current
    form should be considered non functional because it would eventually
    crash chrome

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,7 +14,17 @@ foolishApp.config(function($routeProvider){
    }
  );
 });
-
+foolishApp.run(function($rootScope,$timeout, $log) {
+    $log.log("Things are starting");
+    $rootScope.timeInMs=0;
+    var countUp = function(data) {
+        $rootScope.timeInMs+= 500;
+        $log.log(data + ' - Current time: ' + $rootScope.timeInMs)
+        $timeout(countUp('Not the first time any more'), 500);
+    }
+    $log.log("Start - Current time: 0")
+    $timeout(countUp('First pass'), 500);
+});
 foolishApp.controller('ArtboardCtrl', function() {
   //nothing here yet
 });
