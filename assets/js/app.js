bacc70af49eaa646c398f9f50aaafbc1ec7cbf1b
jeremy <jeremy@digitalminion.com>
2017-02-22 14:25:42 -0600

    Make the astronauts walk
    * Include all the necessary angular modules
    * Include URLs to read, and examples to chew on, for doing theming the Material way
    * In the walker function, set the currentNavItem on the nav menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-var foolishApp = angular.module('foolishApp', ['ngRoute', 'ngAnimate', 'ngMaterial', 'ngAria']);
+var foolishApp = angular.module('foolishApp', ['ngRoute', 'ngMessages', 'ngAnimate', 'ngMaterial', 'ngAria']);
 foolishApp.config(function ($routeProvider) {
     $routeProvider.when("/", {
         templateUrl: "assets/tpl/home.html"
@@ -10,13 +10,42 @@ foolishApp.config(function ($routeProvider) {
         , controllerAs: "about"
     });
 });
-foolishApp.config(function($mdThemingProvider) {
-    $mdThemingProvider.theme('default')
-        .primaryPalette('lime')
-        .warnPalette('red')
-        .accentPalette('blue')
-        .dark();
-});
+
+// See https://material.angularjs.org/1.1.1/api/service/$mdThemingProvider
+// See https://material.angularjs.org/1.1.0/demo/colors for the list of "pallettes" you can use.
+
+//
+// In theory, you could control the theming here, using javascript:
+// But I also specified the color unconditionally in the html 
+//      <md-content class="md-padding" style="background:#66313f" />
+// Unconditionally setting the CSS like this nullifies a lot of the neat animations
+// that Material gives you.  You may want to remove that CSS in the html, and uncomment
+// this, if you want to play around with it.
+//
+//foolishApp.config(function($mdThemingProvider) {
+//    $mdThemingProvider
+//        .theme('docs-dark', 'default')  // Create a theme "docs-dark" copied from "default"
+//        .primaryPalette('cyan')         // (Not sure yet)
+//        .warnPalette('green')           // (Not sure yet)
+//        .accentPalette('grey')          // These are the colors of the text
+//        .backgroundPalette('red')       // These are the colors of the underline, and "highlighted".
+//        //.dark()      // This inverts the colors, in a weird way
+//        ;
+//});
+
+//
+// For more informatino about how to define a pallette (ie, with your own colors),
+// check out https://material.angularjs.org/latest/Theming/03_configuring_a_theme.
+//
+// The colors used by a theme are identified by a number (like "500"), and you can
+// create a new pallette as a clone from an existing one, and redefine the colors.
+// 
+// There are also builtin themes, using builtin pallettees, but you can apparently
+// create a new theme as a clone from an existing one, and replace the pallettes.
+// 
+// All these changes appear to be made through javascript, not CSS.
+//
+
 
 foolishApp.run(function ($rootScope, $timeout, $log) {});
 foolishApp.controller('ArtboardCtrl', function ($scope, $log, $timeout) {
@@ -33,24 +62,28 @@ foolishApp.controller('ArtboardCtrl', function ($scope, $log, $timeout) {
                 s.mercury = false;
                 s.apollo = true;
                 s.mars = true;
+                $scope.currentNavItem="About";
             }
             else if (s.mercury == false) {
                 s.pilot = true;
                 s.mercury = true;
                 s.apollo = false;
                 s.mars = true;
+                $scope.currentNavItem="Portfolio";
             }
             else if (s.apollo == false) {
                 s.pilot = true;
                 s.mercury = true;
                 s.apollo = true;
                 s.mars = false;
+                $scope.currentNavItem="Contact";
             }
             else {
                 s.pilot = false;
                 s.mercury = true;
                 s.apollo = true;
                 s.mars = true;
+                $scope.currentNavItem="Home";
             }
             // Finally, call ourselves again to schedule another step
             step(s)
@@ -87,6 +120,8 @@ foolishApp.controller('ArtboardCtrl', function ($scope, $log, $timeout) {
         //timeout = null;
     }
     
+    // You change this to change the "current" highlighted nav item.
+    // I haven't tested whether you can change it above and have it show up.
     $scope.currentNavItem="Home";
 });
 foolishApp.controller('AboutCtrl', function ($scope, $timeout, $log) {
